a86f5b32082f6327b0619e809d4efbc9af2bb506
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-30 17:09:41 -0300

    Create EntityManagerFactory

---
+++ src/Helper/EntityManagerFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Alura\Doctrine\Helper;
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+
+class EntityManagerFactory
+{
+    public function getEntityManager(): EntityManagerInterface
+    {
+        $rootDir = __DIR__ . '/../..';
+        $config = Setup::createAnnotationMetadataConfiguration(
+            [$rootDir . '/src'], // Diretorio que ira buscar classes com annotations
+            true // dev mode
+        );
+        $connection = [
+            'driver' => 'pdo_sqlite',
+            'path' => $rootDir . '/var/data/banco.sqlite'
+        ];
+        return EntityManager::create($connection, $config);
+    }
+}
